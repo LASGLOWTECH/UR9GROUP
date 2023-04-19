a625f591e3d62e5a5089110b99df23ab1c97b283
LASGLOWTECH <omozemojeau@gmail.com>
2023-04-19 15:48:52 -0700

    made changes to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import { Link } from 'react-router-dom';
 function CONTACTUS() {
   return (
     <Navbar fixed="top" collapseOnSelect expand="lg" id="bakd" className='d-flex justify-content-center' variant="dark">
       <Container>
-        <Navbar.Brand> <img className=" img1" src="/hem.png" alt="image"></img></Navbar.Brand>
+        <Navbar.Brand> <img className=" img1" src="/hem.png" alt="hem"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
        
         <Navbar.Collapse id="responsive-navbar-nav">
